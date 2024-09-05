8124ef4fdd0f300b8f4261ab668447f7a124c4b7
ADRIAN HENRIQUE DE ARAUJO SILVA MORENO <a95416422@rn.senac.local>
2024-09-05 16:09:11 -0300

    criação da página orçamento no electron

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,9 @@ const CdCliente = document.getElementById('cadastroCliente');
 CdCliente.addEventListener('click', () => {
     ipcRenderer.send('abrir-janela-cliente');
 });
+
+const CdOrcamento = document.getElementById('orcamento');
+
+CdOrcamento.addEventListener('click', () => {
+    ipcRenderer.send('abrir-janela-orcamento');
+});
